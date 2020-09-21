b857084c019b77519b8a9cd49e367cdc76b1bd67
Edgar Omar Monreal <edgar.monreal7686@alumnos.udg.mx>
2020-09-21 17:31:46 -0500

    Se captura el nombre completo del usuario y se imprime un saludo

--- Actividad02.cpp
+++ Actividad02.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!" << endl;
+  char nombre[30];
+  cout << "Nombre: "; cin.getline(nombre, sizeof(nombre), '\n');
+  cout << "Hola " << nombre;
 	getchar();
 	return 0;
 }
