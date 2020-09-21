10d1324b55fba8a19d19609ee443c5a59a698588
Edgar Omar Monreal <edgar.monreal7686@alumnos.udg.mx>
2020-09-21 17:46:14 -0500

    Capturamos la edad e imprimimos cuantos anios cumplirá

--- Actividad02.cpp
+++ Actividad02.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
   char nombre[30];
+  unsigned int edad;
+
+	cout << "Hello World!" << endl;
   cout << "Nombre: "; cin.getline(nombre, sizeof(nombre), '\n');
-  cout << "Hola " << nombre;
+  cout << "Hola " << nombre << "!!\n";
+  cout << "Edad: "; cin >> edad;
+  cout << "Vas a cumplir: " << edad + 1 << " anios!!";
 	getchar();
 	return 0;
 }
